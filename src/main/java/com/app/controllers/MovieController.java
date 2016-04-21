330e8cb1bbaa5e66e24da7582c83dba2e9d96866
Marcin Giec <giecmarcin@gmail.com>
2016-04-21 21:09:20 +0200

    Add new vies

--- src/main/java/com/app/controllers/MovieController.java
+++ src/main/java/com/app/controllers/MovieController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Marcin on 21.04.2016.
@@ -24,40 +25,70 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-    @RequestMapping("/add")
-    public String getAddMovieForm(Model model, @RequestParam(value = "id", required = false) Long id){
+    @RequestMapping(value = {"/add", "/add/{id}"})
+    public String getAddMovieForm(Model model, @PathVariable Optional<Long> id) {
         Movie movie = null;
-        if (id == null) {
-            movie = new Movie();
+        if (id.isPresent()) {
+            movie = movieService.findOne(id.get());
         } else {
-            movie = movieService.findOne(id);
+            movie = new Movie();
         }
         model.addAttribute("movie", movie);
         return "/movie/add";
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ModelAndView processAddMovieForm(@ModelAttribute("movie") Movie movie){
-        if(movie.getId()==null){
-            movieService.save(movie);
-        }else{
-            Movie newMovie = movieService.findOne(movie.getId());
-            newMovie = movie;
+    @RequestMapping(value = {"/add", "/add/{id}"}, method = RequestMethod.POST)
+    public ModelAndView processAddMovieForm(@ModelAttribute("movie") Movie movie, @PathVariable Optional<Long> id) {
+        if (id.isPresent()) {
+            Movie newMovie = movieService.findOne(id.get());
+            if(newMovie!=null){
+                newMovie = movie;
+                movieService.save(movie);
+            }else{
+                //Not Found
+            }
+        } else {
             movieService.save(movie);
         }
-        return new ModelAndView("redirect:/movies/all");
+        return new ModelAndView("redirect:/movie/all");
     }
 
+
     @RequestMapping(value = "/all")
-    public String getAllMovies(Model model){
+    public String getAllMovies(Model model) {
         List<Movie> movies = movieService.findAll();
         model.addAttribute("movies", movies);
         return "/movie/index";
     }
 
+    @RequestMapping(value = "/{id}")
+    public String getDetailsOfMovie(Model model, @PathVariable Optional<Long> id){
+        if(id.isPresent()){
+            Movie movie = movieService.findOne(id.get());
+            if(movie!=null){
+                model.addAttribute("movie", movie);
+            }
+        }
+        return "/movie/details";
+    }
+
+    @RequestMapping(value = {"/confirm/{id}"})
+    public String getConfirmTheDelationPage(Model model, @PathVariable Long id){
+        Movie movie = movieService.findOne(id);
+        model.addAttribute("movie", movie );
+        return "/movie/confirm";
+    }
+
+    @RequestMapping(value = {"/remove/{id}"})
+    public ModelAndView removeMovie(@PathVariable Long id){
+        Movie movie = movieService.findOne(id);
+        movieService.delete(movie);
+        return new ModelAndView("redirect:/movie/all");
+    }
+
     @InitBinder("movie")
     public void initialiseBinder(WebDataBinder binder) {
-        binder.setAllowedFields("title", "description", "publicationDate");
+        binder.setAllowedFields("id","title", "description", "publicationDate");
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         CustomDateEditor editor = new CustomDateEditor(dateFormat, true);
         //Register it as custom editor for the Date type

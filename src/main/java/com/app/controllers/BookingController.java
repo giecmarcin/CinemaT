5c4bf44846989bfc0832b806e1672b03634b6e3a
Marcin Giec <giecmarcin@gmail.com>
2016-05-24 12:18:10 +0200

    //

--- src/main/java/com/app/controllers/BookingController.java
+++ src/main/java/com/app/controllers/BookingController.java
@@ -1,7 +1,44 @@
 package com.app.controllers;
 
+import com.app.models.Booking;
+import com.app.models.Seat;
+import com.app.models.Showing;
+import com.app.services.BookingService;
+import com.app.services.ShowingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/booking")
 public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @Autowired
+    ShowingService showingService;
+
+    @RequestMapping(value = {"/add", "/add/{id}"})
+    private ModelAndView getBookingForm(Map<String, Object> modelMap, @PathVariable Optional<Long> id) {
+        System.out.println(id.get());
+        if (id.isPresent()) {
+            Showing showing = showingService.findOne(id.get());
+            if (showing != null) {
+                Booking booking = new Booking();
+                List<Seat> availableSeats = bookingService.findAvailableSeats(id.get());
+                modelMap.put("allseats", showing.getCinemahall().getSeats());
+                modelMap.put("seats", availableSeats);
+                modelMap.put("booking", booking);
+                return new ModelAndView("/booking/add");
+            }
+        }
+        return new ModelAndView("redirect:/");
+    }
 }

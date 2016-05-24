529015157cc5e96a1b5bb36d20b802d0e6d5479c
Marcin Giec <giecmarcin@gmail.com>
2016-05-24 11:14:21 +0200

    Methods for find all availables seats

--- src/main/java/com/app/services/BookingService.java
+++ src/main/java/com/app/services/BookingService.java
@@ -1,6 +1,7 @@
 package com.app.services;
 
 import com.app.models.Booking;
+import com.app.models.Seat;
 import com.app.models.Showing;
 import com.app.models.User;
 
@@ -10,4 +11,6 @@ public interface BookingService extends GenericService<Booking> {
     List<Booking> findByShowing(Showing showing);
 
     List<Booking> findByUser(User user);
+
+    List<Seat> findAvailableSeats(Long idOfShowing);
 }
